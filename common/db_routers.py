ef21046f0c32bd46f52597f9496e26f89eac6a92
황광회 <jack7141@nate.com>
2023-05-26 00:05:58 +0900

    [master] db_router fix

--- common/db_routers.py
+++ common/db_routers.py
@@ -4,10 +4,16 @@ logger = logging.getLogger(__name__)
 
 class MasterSlaveRouter:
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'admin':
-            logger.info(f"Read operation for model {model.__name__} is routed to 'slave' database")
-            return 'slave'
-        return 'default'
+        if model._meta.app_label == 'admin' or \
+                model._meta.app_label == 'sessions' or \
+                model._meta.app_label == 'sites' or \
+                model._meta.app_label == 'auth':
+            logger.info(f"Admin operation(admin, sessions, sites, auth) for model {model.__name__} is routed to 'default' database")
+            return 'default'
+
+        logger.info(f"Read operation for model {model.__name__} is routed to 'slave' database")
+        return 'slave'
+
 
     def db_for_write(self, model, **hints):
         logger.info(f"Write operation for model {model.__name__} is routed to 'default' database")
