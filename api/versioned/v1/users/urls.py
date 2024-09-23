601cd4f0d51f721bbb0a51af6e63d1b614827558
황광회 <jack7141@nate.com>
2024-09-23 22:05:32 +0900

    Auth init

---
+++ api/versioned/v1/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path, include
+from .views import HealthCheckViewSet
+
+urlpatterns = [
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls')),
+]
